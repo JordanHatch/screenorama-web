d7d73de5751334b24f70b1c86055dc3be7092c53
Jordan Hatch <jordan@jordanh.net>
2015-04-20 17:32:07 +0100

    Update the last_requested_at field regularly

---
+++ app/assets/javascripts/display.js
@@ -0,0 +1,25 @@
+(function() {
+  "use strict";
+  window.Screenorama = window.Screenorama || {};
+
+  function Display(options) {
+    this.$el = options.el;
+    this.lastRequestedAtUrl = options.lastRequestedAtUrl;
+
+    this.delay = 30000;
+
+    this.updateLastRequestedAt();
+  }
+
+  Display.prototype.updateLastRequestedAt = function updateLastRequestedAt(){
+    $.ajax({
+      method: 'POST',
+      url: this.lastRequestedAtUrl,
+      data: { update: 1 },
+    });
+
+    setTimeout( $.proxy(this.updateLastRequestedAt, this), this.delay);
+  }
+
+  Screenorama.Display = Display;
+}());
